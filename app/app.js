6b9982c3d9d2aa544fbf38fa83059a60de805cbb
Petr <petr.domkar@gmail.com>
2016-08-18 09:43:44 +0200

    grafika

--- app/app.js
+++ app/app.js
@@ -168,4 +168,8 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
             });
         }
     }
+}).filter('capitalize', function() {
+    return function(input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
 });
