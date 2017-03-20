34f968dbe16d0eac09f92d9397f3a46db46d36f3
Petr <petr.domkar@gmail.com>
2017-03-20 15:01:04 +0100

    upravy

--- app/app.js
+++ app/app.js
@@ -167,7 +167,79 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
             });
         }
     }
-}).filter('capitalize', function() {
+    }).directive('tooltip', function () {
+        return {
+            restrict: 'A',
+            compile: function (element, attributes) {
+                var tag = $('<div style="display: none"></div>');
+                $('body').append(tag);
+
+                return function (scope, element, attr) {
+
+                    var triggerEvent = 'mouseover';
+                    if (attr.tooltipTrigger && attr.tooltipTrigger == 'click') {
+                        triggerEvent = 'click';
+                    }
+
+                    $(element).on(triggerEvent, function (evt) {
+
+                        evt.stopPropagation();
+
+                        var elePos = $(element).offset();
+                        var content = attr.tooltip;
+                        var top = 0, left = 0, xclass = attr.tooltipPos;
+
+                        if (content.substring(0, 1) == '#') {
+                            content = $(content).html();
+                        }
+                        tag.html(content);
+
+                        switch (xclass) {
+                            case 'bottom':
+                                top = elePos.top + tag.height() + 12;
+                                left = elePos.left - 25;
+                                break;
+                            case 'right':
+                                top = elePos.top - 5;
+                                left = elePos.left + $(element).width() + 15;
+                                break;
+                            case 'left':
+                                top = elePos.top - 5;
+                                left = elePos.left - tag.width() - 30;
+                                break;
+                            default:
+                                top = elePos.top - tag.height() - 25;
+                                left = elePos.left - 25;
+                                xclass = ' top';
+                                break;
+                        }
+                        tag.removeClass().addClass('tooltip_box ' + xclass);
+                        if (top < 0) {
+                            top = 0;
+                        }
+                        if (left < 0) {
+                            left = 0;
+                        }
+                        tag.fadeIn(300).css({'top': top, 'left': left});
+
+                        return false;
+                    });
+
+                    if (attr.tooltipDismiss && attr.tooltipDismiss == 'click') {
+                        tag.on('click', function () {
+                            tag.fadeOut(300);
+                        });
+                    } else {
+                        $(element).on('mouseleave', function (evt) {
+
+                            tag.fadeOut(300);
+
+                        });
+                    }
+                }
+            }
+        }
+    }).filter('capitalize', function () {
     return function(input) {
         return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     }
